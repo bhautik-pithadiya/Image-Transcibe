a5322a6bb89afcfe6e3ba2ed001651c25a85a397
bhautik-pithadiya <bhautikpithadiya12@gmail.com>
2024-06-20 05:20:50 +0000

    some changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from PIL import Image
 from fastapi import FastAPI, HTTPException, Request, Form,UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -15,10 +16,27 @@ import asyncio
 import aiohttp
 import uuid
 from io import BytesIO
+from fastapi.responses import JSONResponse
+import os
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://0.0.0.0",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://ec2-44-208-238-252.compute-1.amazonaws.com:5500"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Set up Jinja2 templates
 templates = Jinja2Templates(directory="templates")
@@ -35,6 +53,8 @@ model = LlavaForConditionalGeneration.from_pretrained(
 processor = AutoProcessor.from_pretrained(model_path)
 logger.info("Model Loaded")
 
+
+IMAGE_PATH = 'data/images/'
 def transform_list_of_dicts(data):
     transformed_dict = {}
     
@@ -54,11 +74,12 @@ class ImagePrompt(BaseModel):
 async def get_form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/process_image")
-async def process_image(imageSource: str = Form(...), imageLink: str = Form(None), imageUpload: UploadFile = File(None), prompt: str = Form(...), username: str = Form(...)):
-
+@app.post("/")
+async def process_image(imageSource: str = Form(None),imageLink: str = Form(None), imageUpload: UploadFile = File(None), prompt: str = Form(...), username: str = Form(...)):
+    
     try:
         if imageSource == 'link' and imageLink:
+            unique_id = str(uuid.uuid4())
             logger.info(f"Received form data - ImageLink: {imageLink}, Prompt: {prompt}, Username: {username}")
 
             # Handle multiple image links
@@ -73,18 +94,24 @@ async def process_image(imageSource: str = Form(...), imageLink: str = Form(None
                 response_data["total_processing_time"] = str(total_processing_time)
             else:
                 start_time = datetime.now()
-                response_data = await process_single_image(imageLink, prompt, username)
+                response_data = await process_single_image(imageLink, prompt, username,unique_id)
                 end_time = datetime.now()
                 response_data["total_processing_time"] = str((end_time - start_time).total_seconds())
                 
-        elif imageSource == "upload" and imageUpload:
+        elif imageSource == 'upload' and imageUpload:
+            unique_id = str(uuid.uuid4())
             response_data = {}  
             logger.info(f"Received form data - ImageUpload: {imageUpload.filename}, Prompt: {prompt}, Username: {username}")
             raw_image = Image.open(imageUpload.file)
-            response_data = await process_uploaded_image(raw_image, prompt, username)
+            
+            ## save_image 
+            save_path = os.path.join(IMAGE_PATH,f"{unique_id}.jpg")
+            raw_image.save(save_path)
+            response_data = await process_uploaded_image(raw_image, prompt, username,unique_id)
             
         else:
-            raise HTTPException(status_code=400, detail="Invalid image source or missing data.")
+            response_data =  HTTPException(status_code=400, detail="Invalid image source or missing data.")
+            raise response_data
         
         return response_data
     except Exception as e:
@@ -96,7 +123,7 @@ async def fetch_image(image_link: str):
             image_data = await response.read()
             return Image.open(BytesIO(image_data))
         
-async def process_uploaded_image(raw_image: Image, prompt: str, username: str):
+async def process_uploaded_image(raw_image: Image, prompt: str, username: str,unique_id:str):
     formatted_prompt = f"<image>\n{prompt}\nASSISTANT:"
     inputs = processor(formatted_prompt, raw_image, return_tensors='pt').to(device)
 
@@ -106,7 +133,6 @@ async def process_uploaded_image(raw_image: Image, prompt: str, username: str):
     processing_time = (end_time - start_time).total_seconds()
 
     response = processor.decode(output[0], skip_special_tokens=True)
-    unique_id = str(uuid.uuid4())
     
     response_data = {
         "id": unique_id,
@@ -122,7 +148,7 @@ async def process_uploaded_image(raw_image: Image, prompt: str, username: str):
     return response_data
 
 
-async def process_single_image(image_link: str, prompt: str, username: str):
+async def process_single_image(image_link: str, prompt: str, username: str,unique_id:str):
     raw_image = await fetch_image(image_link)
 
     # Prepare the inputs
@@ -137,7 +163,7 @@ async def process_single_image(image_link: str, prompt: str, username: str):
 
     # Decode the response
     response = processor.decode(output[0], skip_special_tokens=True)
-    unique_id = str(uuid.uuid4())
+    
     # Save the response to a JSON file
     response_data = {
         "id" :str(unique_id),
@@ -169,7 +195,16 @@ def save_data(response_data):
     with open("static/chat_history.json", "w") as file:
         json.dump(chat_history, file)
 
-
+@app.get("/chat_history")
+async def get_chat_history():
+    json_file_path = "static/chat_history.json"
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as file:
+            data = json.load(file)
+        return JSONResponse(content=data)
+    else:
+        return JSONResponse(content={"error": "File not found"}, status_code=404)
+    
 if __name__ == "__main__":
     import uvicorn
     
